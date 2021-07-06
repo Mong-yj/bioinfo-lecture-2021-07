901512787a27dee7d4056bff7059e11b28c889b2
Ryuyeji <yuyae97@naver.com>
2021-07-06 16:39:19 +0900

    day2

--- src/example.py
+++ src/example.py
@@ -56,7 +56,7 @@ while i < 6:
     result *= i
     i += 1
 print(result)
-'''
+
 # 09. function
 def greet():
     print("Hello, Bioinformatics")
@@ -78,4 +78,119 @@ def factorial(n):
         n -= 1
     return val
 print(factorial(5))
-        
+
+# 13. input
+in_value = input("write your name: ")
+print(f"hello {in_value}")
+
+# 14. input2
+in_word = input("write one word: ")
+if in_word.isdigit():
+    print("interger")
+else:               # .isalpha()
+    print("string")
+
+# 15. sys
+import sys
+if len(sys.argv) !=2:
+    print(f"python {sys.argv[0]} [sample]")
+    sys.exit(1)
+
+arg = sys.argv[1]
+print(f"Hello {arg}.")
+
+# 16. file read
+file_name = "read_sample.txt"
+with open(file_name, 'r') as handle:
+    for line in handle:
+        print(line.strip())
+
+handle = open("read_sample.txt",'r')
+#for line in handle:
+#    print(line.strip())
+lines = handle.readlines()
+print(lines)
+for line in lines:
+    print(line.strip())
+handle.close()
+
+# 17. file write
+handle = open("write_sample.txt","w")
+handle.write("Hello\n")
+handle.write("Bioinformatics\n")
+handle.close()
+
+with open("write_sample2.txt",'w') as handle:
+    handle.write("Hello\n")
+
+s = "s1,s2,s3" #csv
+data = s.split(',')
+print(data)
+
+with open("write_sample3.txt", "a") as handle:
+    handle.write("\t".join(data)) #tsv
+
+# 19. Debugging
+try: 
+    with open("naname.txt",'r') as fr:
+        read = fr.readlines()
+        print(read)
+except:
+    print("there are no naname.txt")
+
+# 19-2. Debugging
+import sys
+try:
+    num = int(sys.argv[1])
+except ValueError as err:
+    print(err)
+    sys.exit(2)
+except IndexError as err2:
+    print(err2)
+    sys.exit(3)
+    
+try:
+    num = int(input("Enter: "))
+    print(10/num)
+except ZeroDivisionError as err:
+    print(err)
+    print("0으로 나눌 수 없습니다.")
+except ValueError as err:
+    print(err)
+
+'''
+# 20. string
+a = "Bio"
+b = "Informatics"
+print(a+b)
+
+# 21. string2
+Seq1 = "AGTTTATAG"
+print(Seq1[5])
+# 22. string3
+print(Seq1[3:6])
+# 23.string4
+print(len(Seq1))
+# 24. string5
+Seq1 = "ATGttATaG"
+print(Seq1.upper())
+print(Seq1.lower())
+
+# class
+class A:
+    def __init__(self,val):
+        self.val = val
+    def __add__(self,other):
+        return self.val + other.val
+    def __mul__(self,other):
+        return "__mul__"
+    def __len__(self):
+        return 0
+
+a1 = A(1)
+a2 = A(2)
+print(a1.val + a2.val)
+print(a1+a2) #__add__
+print(a1*a2) #__mul__
+print(len(a1))
+
---
+++ src/example2.py
@@ -0,0 +1,112 @@
+#! /usr/bin/env python
+'''
+# gzip 파일 읽기 by me
+import gzip
+base = ""
+with gzip.open("covid19.fasta.gz",'rb') as f: # binary mode로 읽음
+    for line in f:
+        if line.decode('utf-8').startswith(">"):
+            continue
+        else:
+            base += line.decode('utf-8').strip()
+d_result = {}
+for b in base:
+    if b not in d_result:
+        d_result[b] = 0
+    d_result[b] += 1
+print(d_result)
+
+# 파일 읽기 
+file_name = "covid19.fasta"
+data = {}
+with open(file_name,'r') as handle:
+    for line in handle:
+        if line.startswith(">"):
+            continue
+        for base in line.strip():
+            if base not in data:
+                data[base] = 0
+            data[base] += 1
+print(data)
+
+# gzip by 강사님
+import gzip
+file_name = "covid19.fasta.gz"
+
+data = {}
+with gzip.open(file_name, "rt") as handle: #text mode로 읽음
+    for line in handle:
+        if line.startswith(">"):
+            continue
+        for base in line.strip():
+            if base not in data:
+                data[base] = 0
+            data[base] += 1
+print(data)
+
+# Biopython
+import gzip
+from Bio import SeqIO
+
+f = "covid19.fasta"
+record = SeqIO.read(f, "fasta")
+
+f_gz = "covid19.fasta.gz"
+with gzip.open(f,"rt") as handle:
+    record = SeqIO.read(handle, "fasta")
+'''
+# 25. string
+Seq1 = "ATGTTATAG"
+for i in range(0,len(Seq1),3):
+    print(Seq1[i])
+# 26. string
+for i in range(0,len(Seq1),3):
+    print(Seq1[i:i+3])
+# 27. string
+print(Seq1[::-1])
+# 28. string
+d_base = {'A':'T','G':'C','C':'G','T':'A'}
+comp_seq = ""
+for i in Seq1:
+    comp_seq += d_base[i]
+print(comp_seq)
+
+Seq1 = Seq1.lower()
+Seq1 = Seq1.replace('a','T')
+Seq1 = Seq1.replace('t','A')
+Seq1 = Seq1.replace('c','G')
+Seq1 = Seq1.replace('g','C')
+print(Seq1)
+
+#29. string
+Seq1 = "ATGTTATAG"
+if "C" in Seq1:
+    print("Seq1 have 'C'.")
+else:
+    print("Seq1 have not 'C'.")
+#30. string index
+Seq1 = "AGTTTATAG"
+for i in range(len(Seq1)-2):
+    if Seq1[i:i+2] == 'TT':
+        print(i)
+#31. string split
+seq = "AA,AC,AG,AT"
+l_seq = seq.split(',')
+print(l_seq)
+#32.
+l_seq.append("CA")
+print(l_seq)
+#33.
+print(l_seq[::-1])
+
+#34.
+l_value = [3,1,1,2,0,0,2,3,3]
+print(max(l_value))
+print(min(l_value))
+d_value = {}
+for i in l_value:
+    if i not in d_value:
+        d_value[i] = 0
+    d_value[i] += 1
+print(d_value)
+
